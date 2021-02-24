1766ff839bccc2dc96e56a96f78537aaf6d5b7db
Ibiniyi <ibiniyi.obikoya@globalaccelerex.com>
2021-02-24 01:22:38 +0100

    chore: setup product service

--- src/repositories/interfaces/repository.interface.ts
+++ src/repositories/interfaces/repository.interface.ts
@@ -1,7 +1,7 @@
 import { BaseQueryDto } from '../../dtos/base.query.dto';
 
-export interface IReposiory<T> {
+export interface IRepository<T> {
   findAll(query?: BaseQueryDto): Promise<T[]>;
   save(t: T): Promise<T>;
-  update(t: T, id: number): Promise<T>;
+  update(t: T, id: number): Promise<boolean>;
 }
